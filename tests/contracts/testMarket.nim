d369a6534c89b4ae3c0df3cc4dc4a1940646df39
Arnaud <arnaud@status.im>
2025-04-04 15:08:41 +0200

    Try to fix tests on CI

--- tests/contracts/testMarket.nim
+++ tests/contracts/testMarket.nim
@@ -587,7 +587,7 @@ ethersuite "On-Chain Market":
     switchAccount(host)
     await market.reserveSlot(request.id, 0.uint64)
     await market.fillSlot(request.id, 0.uint64, proof, request.ask.collateralPerSlot)
-    let filledAt = (await ethProvider.currentTime()) - 1.u256
+    let filledAt = await ethProvider.currentTime()
 
     for slotIndex in 1 ..< request.ask.slots:
       await market.reserveSlot(request.id, slotIndex.uint64)
@@ -614,7 +614,7 @@ ethersuite "On-Chain Market":
     switchAccount(host)
     await market.reserveSlot(request.id, 0.uint64)
     await market.fillSlot(request.id, 0.uint64, proof, request.ask.collateralPerSlot)
-    let filledAt = (await ethProvider.currentTime()) - 1.u256
+    let filledAt = await ethProvider.currentTime()
 
     for slotIndex in 1 ..< request.ask.slots:
       await market.reserveSlot(request.id, slotIndex.uint64)
